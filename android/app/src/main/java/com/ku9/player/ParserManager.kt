55547219e20ad10237891b420286d31cd5832afc
tytestelle <ccszdk@gmail.com>
2026-07-21 00:44:16 +0800

    Update ParserManager.kt

--- android/app/src/main/java/com/ku9/player/ParserManager.kt
+++ android/app/src/main/java/com/ku9/player/ParserManager.kt
@@ -1,6 +1,10 @@
 package com.ku9.player
 
 class ParserManager {
-    fun parseM3U(content: String): List<Channel> = M3UParser().parse(content)
+
+    fun parseM3U(content: String): List<Group> { // 添加泛型 <Group>
+        return M3UParser().parse(content)
+    }
+
     // 可添加其他格式解析
 }
