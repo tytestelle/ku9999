df789623812b17d496f66aed48ea640655059cf0
tytestelle <ccszdk@gmail.com>
2026-07-20 22:27:15 +0800

    Delete Group.kt

--- Group.kt
+++
@@ -1,8 +0,0 @@
-package com.ku9.player
-
-data class Group(
-    val id: String = "",
-    val name: String = "",
-    val channels: List<Channel> = emptyList(),
-    val subGroups: List<Group> = emptyList()   // 二级分组
-)
