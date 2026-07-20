57e9ecc9a5d7577aa40f43548b9fa3e1b4c8b476
tytestelle <ccszdk@gmail.com>
2026-07-20 21:05:57 +0800

    Update Channel.kt

--- Channel.kt
+++ Channel.kt
@@ -1,3 +1,12 @@
 package com.ku9.player
 
-data class Channel(val id: String = "", val name: String = "", val url: String = "")
+data class Channel(
+    val id: String = "",
+    val name: String = "",
+    val url: String = "",
+    val backupUrls: List<String> = emptyList(),  // 备用源
+    val logoUrl: String = "",                    // 台标
+    val epgUrl: String = "",                     // EPG URL
+    val headers: Map<String, String> = emptyMap(), // 自定义请求头
+    val groupId: String = ""                     // 所属分组
+)
