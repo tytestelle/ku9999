fe78e96d8d07df10d56ec8aee9c56fa047ceddd5
tytestelle <ccszdk@gmail.com>
2026-07-21 00:42:53 +0800

    Update MainActivity.kt

--- android/app/src/main/java/com/ku9/player/MainActivity.kt
+++ android/app/src/main/java/com/ku9/player/MainActivity.kt
@@ -7,33 +7,7 @@ import android.view.ViewGroup
 import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
-class ChannelListFragment : Fragment() {
-
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var searchView: SearchView
-    private lateinit var adapter: ChannelAdapter
-    private val sourceManager by lazy { SourceManager(requireContext()) }
-    private var allChannels: List<Channel> = emptyList()
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_channel_list, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        recyclerView = view.findViewById(R.id.channel_recycler)
-        searchView = view.findViewById(R.id.search_view)
-
-        adapter = ChannelAdapter { channel ->
-            val app = requireContext().applicationContext as MyApplication
-            app.playerManager.play(channel.url, mapOf("User-Agent" to "Ku9Player"))
-        }
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.adapter = adapter
 
