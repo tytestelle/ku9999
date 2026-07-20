bbe26c2bf92d7a2b6b7a73c9c74b5072d75773bb
tytestelle <ccszdk@gmail.com>
2026-07-21 00:40:55 +0800

    Update Group.kt

--- android/app/src/main/java/com/ku9/player/Group.kt
+++ android/app/src/main/java/com/ku9/player/Group.kt
@@ -3,6 +3,6 @@ package com.ku9.player
 data class Group(
     val id: String = "",
     val name: String = "",
-    val channels: List<Channel> = emptyList(),
-    val subGroups: List<Group> = emptyList()   // 二级分组
+    val channels: List<Channel> = emptyList(), // 添加泛型 <Channel>
+    val subGroups: List<Group> = emptyList() // 二级分组 - 添加泛型 <Group>
 )
