cd26b6e7425333a3852c722237503c9f45a9602d
tytestelle <ccszdk@gmail.com>
2026-07-20 21:06:16 +0800

    Update Group.kt

--- Group.kt
+++ Group.kt
@@ -1,3 +1,8 @@
 package com.ku9.player
 
-data class Group(val id: String = "", val name: String = "", val channels: List<Channel> = emptyList())
+data class Group(
+    val id: String = "",
+    val name: String = "",
+    val channels: List<Channel> = emptyList(),
+    val subGroups: List<Group> = emptyList()   // 二级分组
+)
