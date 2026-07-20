f0003a8e7d3a10ed272efafee58e370070cb4912
tytestelle <ccszdk@gmail.com>
2026-07-20 20:21:05 +0800

    Update Group.kt

--- Group.kt
+++ Group.kt
@@ -1,7 +1,3 @@
 package com.ku9.player
 
-data class Group(
-    val id: String = "",
-    val name: String = "",
-    val channels: List<Channel> = emptyList()
-)
+data class Group(val id: String = "", val name: String = "", val channels: List<Channel> = emptyList())
