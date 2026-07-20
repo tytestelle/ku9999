4468b5afe0f44e7b647650586e3ad751278ff9b6
tytestelle <ccszdk@gmail.com>
2026-07-20 20:19:39 +0800

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.launch
 
-// ---------- 扩展函数：显示 Toast ----------
 fun MainActivity.showToast(msg: String) {
     Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
 }
@@ -23,20 +22,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 假设布局中有两个 RecyclerView，ID 分别为 rv_channels 和 rv_groups
         val rvChannels = findViewById<RecyclerView>(R.id.rv_channels)
         val rvGroups = findViewById<RecyclerView>(R.id.rv_groups)
 
         channelAdapter = ChannelAdapter { channel ->
             lifecycleScope.launch {
-                // 处理频道点击，例如加载播放
-                showToast("点击频道：${channel.name}")
+                showToast("点击频道: ${channel.name}")
+                // 这里可以调用播放器
             }
         }
         groupAdapter = GroupAdapter { group ->
             lifecycleScope.launch {
-                // 处理分组点击
-                showToast("点击分组：${group.name}")
+                showToast("点击分组: ${group.name}")
             }
         }
 
@@ -45,10 +42,8 @@ class MainActivity : AppCompatActivity() {
         rvGroups.layoutManager = LinearLayoutManager(this)
         rvGroups.adapter = groupAdapter
 
-        // 加载数据
         lifecycleScope.launch {
             sourceManager.loadData()
-            // 假设 sourceManager 有 channels 和 groups 公开属性
             channelAdapter.setData(sourceManager.channels)
             groupAdapter.setData(sourceManager.groups)
         }
