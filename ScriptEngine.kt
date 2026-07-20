c538469d71370b967bdd6d95d4855474ca427bad
tytestelle <ccszdk@gmail.com>
2026-07-20 22:37:20 +0800

    Delete ScriptEngine.kt

--- ScriptEngine.kt
+++
@@ -1,9 +0,0 @@
-package com.ku9.player
-
-// 仅示意，需集成 Rhino 或 J2V8 引擎
-class ScriptEngine {
-    fun execute(script: String, input: String): String {
-        // 执行JS脚本，处理URL
-        return input
-    }
-}
